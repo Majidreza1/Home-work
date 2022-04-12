b6484f8a2440bab84385d451862e0d1cd5e97343
Majidreza1 <100372293+Majidreza1@users.noreply.github.com>
2022-04-12 12:08:31 +0430

    Add files via upload

---
+++ b2.java
@@ -0,0 +1,11 @@
+
+public class Rectangular {
+    public int tool;
+    public int arz;
+    int area;
+
+    public int areaofrectangular( int a , int b ){
+        area = a * b;
+        return area;
+    }
+}
